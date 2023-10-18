95cdbe42f04cce4e82875105812bf59d78cafcce
Amruthamsh A <32568328+Amruthamsh@users.noreply.github.com>
2023-10-18 09:27:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+var top_nav = document.getElementById("myTopnav");
+var iconElement = document.querySelector('.icon i');
+var nav = document.querySelector('.navbar-top');
+var navbar_open = false;
+
+function myFunction() {
+  if (top_nav.className === "navbar-top") {
+    top_nav.className += " responsive";
+    navbar_open = true;
+    iconElement.className = 'fa fa-car';
+  } else {
+    top_nav.className = "navbar-top";
+    navbar_open = false;
+    iconElement.className = 'fa fa-bars';
+  }
+}
+
+function isInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+document.addEventListener('scroll', function () {
+    if(!isInViewport(nav))
+        top_nav.className = "navbar-top";
+    else if(navbar_open)
+        top_nav.className += " responsive";
+}, {
+  passive: true
+});
+
