d256fe4868650d721f01ace6f7fff05b87c04f0a
Amruthamsh A <32568328+Amruthamsh@users.noreply.github.com>
2023-11-01 23:04:37 +0530

    formatting changes

--- contact-header.js
+++ contact-header.js
@@ -1,15 +1,16 @@
-const template = document.createElement('template');
+const template = document.createElement("template");
 
 template.innerHTML = `
 <div class="contact-info">
-	    <img src="icons/phone.svg">
-            <a href="tel:1800-889-3916">1800-889-3916</a>
-            <img src="icons/mobile.svg">
-            <a href="tel:+91 - 80 35712085">+91 - 80 35712085</a>
-            <img src="icons/mail.png">
-            <a href="mailto:info@sureworks.in">info@sureworks.in</a>
+	<img src="icons/phone.svg">
+    <a href="tel:1800-889-3916">1800-889-3916</a>
+
+    <img src="icons/mobile.svg">
+    <a href="tel:+91 - 80 35712085">+91 - 80 35712085</a>
+    
+    <img src="icons/mail.png">
+    <a href="mailto:info@sureworks.in">info@sureworks.in</a>
 </div>
 `;
 
 document.body.appendChild(template.content);
-
--- script.js
+++ script.js
@@ -1,55 +1,49 @@
 var top_nav = document.getElementById("myTopnav");
-var iconElement = document.querySelector('.icon i');
-var nav = document.querySelector('.navbar-top');
+var iconElement = document.querySelector(".icon i");
+var nav = document.querySelector(".navbar-top");
 var navbar_open = false;
 
 function myFunction() {
   if (top_nav.className === "navbar-top") {
     top_nav.className += " responsive";
     navbar_open = true;
-    iconElement.className = 'fa fa-close';
+    iconElement.className = "fa fa-close";
   } else {
     top_nav.className = "navbar-top";
     navbar_open = false;
-    iconElement.className = 'fa fa-bars';
+    iconElement.className = "fa fa-bars";
   }
 }
 
 function isInViewport(el) {
   const rect = el.getBoundingClientRect();
   return (
-      rect.top >= -80 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    rect.top >= -80 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
 
-document.addEventListener('scroll', function () {
-    if(!isInViewport(nav))
-        top_nav.className = "navbar-top";
-    else if(navbar_open)
-        top_nav.className += " responsive";
-}, {
-  passive: true
-});
-
-
+document.addEventListener(
+  "scroll",
+  function () {
+    if (!isInViewport(nav)) top_nav.className = "navbar-top";
+    else if (navbar_open) top_nav.className += " responsive";
+  },
+  {
+    passive: true,
+  }
+);
 
 function validateForm() {
   let name = document.forms["get-in-touch"]["user-name"].value;
-let email= document.forms["get-in-touch"]["user-email"].value;
-let subject= document.forms["get-in-touch"]["user-subject"].value;
+  let email = document.forms["get-in-touch"]["user-email"].value;
+  let subject = document.forms["get-in-touch"]["user-subject"].value;
   console.log(name);
   if (name == "" || email == "" || subject == "") {
     alert("Incomplete information!");
     return false;
   }
-  //let text = "Hi " + name + "! Thanks for dropping by, your message will be conveyed."
-  //document.getElementById("submission-complete").innerHTML = text;
 }
-
-
-
-
-
